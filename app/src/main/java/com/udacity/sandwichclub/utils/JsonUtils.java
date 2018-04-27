c4e60207bba2a694f795f06784e93f3c027f69fd
EricRybarczyk <EricRybarczyk@gmail.com>
2018-04-26 20:06:57 -0400

    Implemented JSON Parsing to Sandwich Object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,58 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich requestedSandwich;
+
+        JSONObject sandwichDataObject, nameDataObject;
+        JSONArray alsoKnownAsList, ingredientList;
+
+        String mainName;
+        List<String> alsoKnownAs = new ArrayList<String>() {};
+        List<String> ingredients = new ArrayList<String>() {};
+        String description;
+        String placeOfOrigin;
+        String image;
+
+        try {
+            sandwichDataObject = new JSONObject(json);
+            nameDataObject = new JSONObject(sandwichDataObject.getString("name"));
+
+            mainName = nameDataObject.getString("mainName");
+            alsoKnownAsList = nameDataObject.getJSONArray("alsoKnownAs");
+            placeOfOrigin = sandwichDataObject.getString("placeOfOrigin");
+            description = sandwichDataObject.getString("description");
+            image = sandwichDataObject.getString("image");
+            ingredientList = sandwichDataObject.getJSONArray("ingredients");
+
+            for (int i = 0; i < ingredientList.length(); i++) {
+                ingredients.add(ingredientList.getString(i));
+            }
+
+            for (int i = 0; i < alsoKnownAsList.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsList.getString(i));
+            }
+
+
+            requestedSandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+        return requestedSandwich;
     }
 }
